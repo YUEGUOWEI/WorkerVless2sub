91c9636847a3fa56a752117e5cd61ce7a524ae46
YUEGUOWEI <45186187+YUEGUOWEI@users.noreply.github.com>
2024-04-05 09:44:28 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,13 +5,13 @@ let mytoken= ['auto'];//快速订阅访问入口, 留空则不启动快速订阅
 
 // 设置优选地址，不带端口号默认443，TLS订阅生成
 let addresses = [
-	'icook.tw:2053#官方优选域名',
-	'cloudflare.cfgo.cc#优选官方线路',
+	//'icook.tw:2053#官方优选域名',
+	//'cloudflare.cfgo.cc#优选官方线路',
 ];
 
 // 设置优选地址api接口
 let addressesapi = [
-	'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
+	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
 	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesipv6api.txt', //IPv6优选内容格式 自行搭建。
 ];
 
@@ -20,6 +20,18 @@ let addressesnotls = [
 	'www.visa.com.sg#官方优选域名',
 	'www.wto.org:8080#官方优选域名',
 	'www.who.int:8880#官方优选域名',
+	'icook.tw:2053#优选域名',
+	'cloudflare.cfgo.cc#优选官方线路',
+	'icook.hk:8443#香港',
+	'cfip.xxxxxxxx.tk#官方优选-otc提供',
+	'hk.100366.xyz#香港',
+	'acus.cloudflarest.link:80#优选域名1',
+	'acsg.cloudflarest.link:2052#优选域名2',
+	'acjp2.cloudflarest.link:2052#优选域名3',
+	'acsg3.cloudflarest.link:2052#优选域名4',
+	'achk.cloudflarest.link:2052#优选域名5',
+	'acjp.cloudflarest.link:2052#优选域名6',
+	'a.ygw645379106.eu.org#优选域名a',
 ];
 
 // 设置优选noTLS地址api接口
@@ -32,7 +44,7 @@ let addressescsv = [
 	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressescsv.csv', //iptest测速结果文件。
 ];
 
-let subconverter = "apiurl.v1.mk"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
+let subconverter = "ding-yue-zhuan-huan.ygw645379106.workers.dev"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
 let subconfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_Full_MultiMode.ini"; //订阅转换配置文件
 let noTLS = false; //改为 true , 将不做域名判断 始终返回noTLS节点
 let link = '';
